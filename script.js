19e72dc537f216d28d0d56b16a0275cc1ef0dde9
RICARDO P. DO NASCIMENTO <59373431+RicardoPNascimento@users.noreply.github.com>
2022-01-25 21:16:17 -0300

    Add files via upload
    2-Bootstrap adicionado , botões adicionados (adicionar,excluir,apagar tudo) , falta finalizar botão editar.

--- script.js
+++ script.js
@@ -1,6 +1,103 @@
 //linkando o botao adicionar
+let produtos = [];
+let i = 0;
+let botaoSalvar=document.getElementById('botaoSalvar')
+let botaoCancelar=document.getElementById('botaoCancelar')
+let botaoAdicionar=document.getElementById('botaoAdicionar')
 
-function adiconar(){
+function adicionar(){
     let produto = document.querySelector("#inputProduto").value
+    let verTela = document.querySelector(".listaProduto")
+    produtos.push(produto)
+    
+
+   verTela.innerHTML +=`
+  
+       
+  
+          <tr>
+            
+            <td>${produto}</td>
+            <td></td>
+            <td></td>
+            <td>
+              <button onclick="editar('${produtos[i]}')" id="icbutton" data-bs-toggle="modal" data-bs-target="" class="btn btn-dark">Editar</button>
+              <button  onclick="Limpar('${produtos[i]}')" id="deletebutton" class="btn btn-dark">Excluir</button>
+            </td>
+          </tr>
+        
+      
+   `
+   
+   i++;
     console.log(produto)
 }
+
+function Limpar(name_product){
+    let indice = produtos.indexOf(name_product)
+    produtos.splice(indice,1)
+    let refresh = 0
+    let verTela = document.querySelector(".listaProduto")
+    verTela.innerHTML=''
+    while (refresh<produtos.length){
+         verTela.innerHTML +=`
+         
+         <tr>
+         <td>${produtos[refresh]}</td>
+         <td></td>
+         <td></td>
+         <td>
+           <button id="icbutton" data-bs-toggle="modal" data-bs-target="">Editar</button>
+           <button  onclick="Limpar('${produtos[refresh]}')" id="deletebutton">Excluir</button>
+         </td>
+       </tr>
+     
+     
+      `
+      refresh++
+    }
+      
+      
+}
+
+function limpartudo(){
+    for (x=0;x=produtos.length;x++){
+        produtos.pop(x)
+        console.log('excluiu')
+    }
+    let verTela = document.querySelector(".listaProduto")
+    verTela.innerHTML=''
+}
+
+function editar(){
+    
+    botaoSalvar.style.display='inline-block'
+    botaoCancelar.style.display='inline-block'
+    botaoAdicionar.style.display='none'
+}
+
+function cancelar(){
+    
+    botaoSalvar.style.display='none'
+    botaoCancelar.style.display='none'
+    botaoAdicionar.style.display='inline-block'
+}
+
+function salvar(name_product){
+    let indice = produtos.indexOf(name_product)
+    
+    botaoSalvar.style.display='none'
+    botaoCancelar.style.display='none'
+    botaoAdicionar.style.display='inline-block'
+    console.log(indice)
+}
+
+
+
+
+
+
+
+
+
+
